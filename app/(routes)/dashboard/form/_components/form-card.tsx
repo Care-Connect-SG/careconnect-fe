df0f8697c6ae02b659c6638ace10105097fe35d7
notsushiii <sushweyeemdy@gmail.com>
2025-03-01 13:03:32 +0800

    feat: edit, delete, publish form

--- app/(routes)/dashboard/form/_components/form-card.tsx
+++ app/(routes)/dashboard/form/_components/form-card.tsx
@@ -2,30 +2,42 @@ import { Badge } from "@/components/ui/badge";
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-// import { Eye, FilePenLine } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { BookPlus, FilePenLine, Trash2 } from "lucide-react";
+import Link from "next/link";
 
 interface FormCardProps {
+  id: string;
   title: string;
   description: string;
   created_date: string;
   status: string;
+  onPublish: (formId: string) => void;
+  onDelete: (formId: string) => void;
 }
 
 export default function FormCard({
+  id,
   title,
   description,
   created_date,
   status,
+  onPublish,
+  onDelete,
 }: FormCardProps) {
   return (
     <Card
-      className={`w-xs max-w-xs h-[10rem] overflow-hidden ${
-        status === "Published"
+      className={`w-xs max-w-xs h-[11rem] overflow-hidden ${
+        status == "Published"
           ? "border-l-4 border-l-green-500"
           : "border-l-4 border-l-yellow-500"
       }`}
@@ -52,12 +64,49 @@ export default function FormCard({
       <CardContent>
         <p className="hidden md:block text-xs truncate">{description}</p>
       </CardContent>
-      {/* <CardFooter className={`flex justify-end gap-2`}>
-                {status === "Draft" && (
-                    <FilePenLine className="h-4 w-4" />
-                )}
-                <Eye className="h-4 w-4" />
-            </CardFooter> */}
+      <CardFooter className="flex justify-end gap-4 py-2 px-6 text-semibold text-gray-400">
+        {status === "Draft" && (
+          <div className="flex gap-4 items-center">
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Link href={`form/build?id=${id}`}>
+                    <FilePenLine className="h-4 w-4 hover:text-gray-600" />
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Edit</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <button onClick={() => onPublish(id)}>
+                    <BookPlus className="h-4 w-4 hover:text-gray-600" />
+                  </button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Publish</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+            <div className="w-[1.5px] h-5 bg-gray-300"></div>
+          </div>
+        )}
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <button onClick={() => onDelete(id)}>
+                <Trash2 className="h-4 w-4 hover:text-gray-600" />
+              </button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Delete</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </CardFooter>
     </Card>
   );
 }

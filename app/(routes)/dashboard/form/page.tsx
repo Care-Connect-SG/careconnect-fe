0f8e319999b81b06a31f67956697740a1126777c
notsushiii <sushweyeemdy@gmail.com>
2025-03-04 01:48:31 +0800

    feat: page for incident reporting forms

--- app/(routes)/dashboard/form/page.tsx
+++ app/(routes)/dashboard/form/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 
-import { deleteForm, getForms, publishForm } from "@/app/api/form";
-import { Card } from "@/components/ui/card";
+import { getForms } from "@/app/api/form";
 import { FormComplete } from "@/types/form";
-import { Plus } from "lucide-react";
-import Link from "next/link";
 import { useEffect, useState } from "react";
-import FormCard from "./_components/form-card";
+import FormCardPublished from "./_components/form-card-published";
+
 
 const formatDate = (isoDate: string): string => {
   const date = new Date(isoDate);
@@ -17,7 +15,7 @@ const formatDate = (isoDate: string): string => {
   }).format(date);
 };
 
-export default function Incident() {
+export default function Report() {
   const [forms, setForms] = useState<FormComplete[]>([]);
 
   useEffect(() => {
@@ -26,31 +24,10 @@ export default function Incident() {
 
   const fetchForms = async () => {
     try {
-      const data = await getForms();
-      console.log(data);
+      const data = await getForms("Published");
       setForms(data);
     } catch (error) {
       console.error("Failed to fetch forms");
-    }
-  };
-
-  const handlePublish = async (formId: string) => {
-    try {
-      await publishForm(formId);
-      console.log("Published form: ", formId);
-      fetchForms();
-    } catch (error) {
-      console.error("Failed to publish form");
-    }
-  };
-
-  const handleDelete = async (formId: string) => {
-    try {
-      await deleteForm(formId);
-      console.log("Deleted form: ", formId);
-      fetchForms();
-    } catch (error) {
-      console.error("Failed to delete form");
     }
   };
 
@@ -58,35 +35,24 @@ export default function Incident() {
     <>
       <div className="px-8 py-4">
         <h1 className="scroll-m-20 text-2xl font-semibold tracking-tight py-2">
-          Incident Reporting Form Studio
+          Incident Reporting
         </h1>
         <p className="text-sm text-muted-foreground">
-          Create, review and publish your incident reporting forms.
+          Select a suitable form to use for your incident reporting needs
         </p>
       </div>
       <hr className="border-t-1 border-gray-300 mx-8 py-2"></hr>
       <div className="w-full grid grid-cols-3 gap-4 px-8 py-4">
         {forms.map((form) => (
           <div key={form._id} onClick={(e) => e.preventDefault()}>
-            <FormCard
+            <FormCardPublished
               id={form._id}
               title={form.title}
               description={form.description}
               created_date={formatDate(form.created_date)}
-              status={form.status}
-              onPublish={handlePublish}
-              onDelete={handleDelete}
             />
           </div>
         ))}
-        <Card className="border-dashed w-xs max-w-xs h-[11rem] hover:bg-gray-100">
-          <Link href="/dashboard/form/build">
-            <div className="flex flex-col justify-center items-center h-full">
-              <Plus className="mb-0 h-6 w-6 text-gray-500" />
-              <p className="text-sm text-gray-400 mt-1">Create a new form</p>
-            </div>
-          </Link>
-        </Card>
       </div>
     </>
   );

196d5df9481a0a7c0484f5833fdb25915ec3f8d3
notsushiii <sushweyeemdy@gmail.com>
2025-03-06 00:03:27 +0800

    feat: add middleware to protect /incident/admin

---
+++ middleware.ts
@@ -0,0 +1,46 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+import { Role } from "./types/user";
+
+export async function middleware(req: NextRequest) {
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+    if (req.nextUrl.pathname.startsWith("/auth/login")) {
+        return NextResponse.next();
+    }
+
+    if (!token || !token.accessToken) {
+        return NextResponse.redirect(new URL("/auth/login", req.url));
+    }
+
+    const restrictedRoutes: Record<string, string> = {
+        "/dashboard/incident/admin": Role.ADMIN,
+    };
+
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BE_API_URL}/users/me/role`, {
+            headers: { Authorization: `Bearer ${token.accessToken}` },
+        });
+
+        if (!res.ok) throw new Error("Failed to fetch user role");
+
+        const userRole = await res.json();
+        const role = userRole.role;
+        console.log("User role:", role);
+
+        for (const [path, requiredRole] of Object.entries(restrictedRoutes)) {
+            if (req.nextUrl.pathname.startsWith(path) && role !== requiredRole) {
+                return NextResponse.redirect(new URL("/dashboard/home", req.url));
+            }
+        }
+    } catch (error) {
+        console.error("Error fetching user role:", error);
+        return NextResponse.redirect(new URL("/auth/login", req.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ["/dashboard/incident/admin"],
+};

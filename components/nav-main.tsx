5dc51d584fe51baa884a62ea0e80b8b16c435611
notsushiii <sushweyeemdy@gmail.com>
2025-03-07 03:39:15 +0800

    feat: implement incident reports sidebar

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -2,35 +2,71 @@
 
 import {
   SidebarGroup,
-  SidebarGroupLabel,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
 } from "@/components/ui/sidebar";
+import { Collapsible } from "@radix-ui/react-collapsible";
 import { LucideIcon } from "lucide-react";
 import Link from "next/link";
+import { CollapsibleContent, CollapsibleTrigger } from "./ui/collapsible";
+
+
+interface submenuItem {
+  title: string;
+  url: string;
+}
 
 export function NavMain({
   items,
 }: {
   items: {
     title: string;
-    url: string;
+    url?: string;
     icon?: LucideIcon;
+    submenu?: submenuItem[];
   }[];
 }) {
   return (
     <SidebarGroup>
       <SidebarMenu>
         {items.map((item) => (
-          <SidebarMenuItem key={item.title}>
-            <Link href={item.url} passHref legacyBehavior>
-              <SidebarMenuButton tooltip={item.title}>
-                {item.icon && <item.icon />}
-                <span>{item.title}</span>
-              </SidebarMenuButton>
-            </Link>
-          </SidebarMenuItem>
+          item.title === "Incident Reports" ?
+            <Collapsible key={item.title}>
+              <SidebarMenuItem>
+                <CollapsibleTrigger asChild>
+                  <SidebarMenuButton tooltip={item.title}>
+                    {item.icon && <item.icon />}
+                    <span>{item.title}</span>
+                  </SidebarMenuButton>
+                </CollapsibleTrigger>
+                <CollapsibleContent>
+                  <SidebarMenuSub>
+                    {item.submenu?.map((subItem) => (
+                      <SidebarMenuSubItem key={subItem.title}>
+                        <SidebarMenuSubButton asChild>
+                          <a href={subItem.url}>
+                            <span>{subItem.title}</span>
+                          </a>
+                        </SidebarMenuSubButton>
+                      </SidebarMenuSubItem>
+                    ))}
+                  </SidebarMenuSub>
+                </CollapsibleContent>
+              </SidebarMenuItem>
+            </Collapsible>
+            :
+            <SidebarMenuItem key={item.title}>
+              <Link href={item.url!} passHref legacyBehavior>
+                <SidebarMenuButton tooltip={item.title}>
+                  {item.icon && <item.icon />}
+                  <span>{item.title}</span>
+                </SidebarMenuButton>
+              </Link>
+            </SidebarMenuItem>
         ))}
       </SidebarMenu>
     </SidebarGroup>

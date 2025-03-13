f2b2cb30e96ddcdac4f7f15ccccdd9f285b9a77e
notsushiii <sushweyeemdy@gmail.com>
2025-03-13 13:02:23 +0800

    feat: add involved residents and caregivers

--- app/(routes)/dashboard/incidents/view/page.tsx
+++ app/(routes)/dashboard/incidents/view/page.tsx
@@ -3,6 +3,7 @@
 import { getFormById } from "@/app/api/form";
 import { getReportById } from "@/app/api/report";
 import { getResidentById } from "@/app/api/resident";
+import { getUserById } from "@/app/api/user";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
@@ -11,13 +12,12 @@ import { formatDayMonthYear } from "@/lib/utils";
 import { FormResponse } from "@/types/form";
 import { ReportResponse } from "@/types/report";
 import { ResidentRecord } from "@/types/resident";
+import { User, UserResponse } from "@/types/user";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { LoadingSkeleton } from "../_components/loading-skeleton";
-import { User, UserResponse } from "@/types/user";
-import { getUserById } from "@/app/api/user";
 
 export default function ViewReportPage() {
   const router = useRouter();
@@ -99,40 +99,68 @@ export default function ViewReportPage() {
         <CardContent>
           <div>
             <div className="flex my-4">
-              {resident && (
-                <div className="w-1/2">
-                  <h2 className="text-gray-500 font-semibold mb-2">
-                    Primary Resident
-                  </h2>
-                  <Link
-                    href={`/dashboard/residents/${resident.id}`}
-                    className="flex items-center gap-3 group hover"
-                  >
-                    <Avatar className="h-16 w-16 group-hover:ring-1 group-hover:ring-primary group-hover:ring-offset-2 transition-all">
-                      <AvatarFallback>PR</AvatarFallback>
-                    </Avatar>
-                    <div>
-                      <div className="font-medium flex items-center gap-1">
-                        <span className="border-b border-dotted border-muted-foreground group-hover:border-primary mb-1">
-                          {resident.full_name}
-                        </span>
-                        <ChevronRight className="h-4 w-4 mb-1 opacity-0 group-hover:opacity-100 transition-opacity" />
+              {(resident ||
+                (report?.involved_residents &&
+                  report?.involved_residents.length > 0)) && (
+                  <div className="w-1/2">
+                    {resident && (
+                      <div className="mb-4">
+                        <h2 className="text-gray-500 font-semibold mb-2">
+                          Primary Resident
+                        </h2>
+                        <div>
+                          <Link
+                            href={`/dashboard/residents/${resident.id}`}
+                            className="flex items-center gap-3 group hover"
+                          >
+                            <Avatar className="h-16 w-16 group-hover:ring-1 group-hover:ring-primary group-hover:ring-offset-2 transition-all">
+                              <AvatarFallback>PR</AvatarFallback>
+                            </Avatar>
+                            <div>
+                              <div className="font-medium flex items-center gap-1">
+                                <span className="border-b border-dotted border-muted-foreground group-hover:border-primary mb-1">
+                                  {resident.full_name}
+                                </span>
+                                <ChevronRight className="h-4 w-4 mb-1 opacity-0 group-hover:opacity-100 transition-opacity" />
+                              </div>
+                              <p className="text-sm text-muted-foreground">
+                                Room: {resident.room_number}
+                              </p>
+                              <p className="text-sm text-muted-foreground">
+                                DOB: {resident.date_of_birth}
+                              </p>
+                            </div>
+                          </Link>
+                        </div>
                       </div>
-                      <p className="text-sm text-muted-foreground">
-                        Room: {resident.room_number}
-                      </p>
-                      <p className="text-sm text-muted-foreground">
-                        DOB: {resident.date_of_birth}
-                      </p>
-                    </div>
-                  </Link>
-                </div>
-              )}
+                    )}
+                    {report?.involved_residents &&
+                      report?.involved_residents.length > 0 && (
+                        <div>
+                          <h2 className="text-gray-500">Involved residents:</h2>
+                          <div>
+                            {report.involved_residents.map((ir, index) => (
+                              <span key={ir.id}>
+                                <Link
+                                  href={`/dashboard/residents/${ir.id}`}
+                                  className="border-b border-dotted border-muted-foreground"
+                                >
+                                  {ir.name}
+                                </Link>
+                                {index < report.involved_residents!.length - 1 &&
+                                  ", "}
+                              </span>
+                            ))}
+                          </div>
+                        </div>
+                      )}
+                  </div>
+                )}
               <div className="w-1/2">
                 <h2 className="text-gray-500 font-semibold mb-2">Reporter</h2>
                 <Link
                   href={`/dashboard/nurses/${report?.reporter.id}`}
-                  className="flex items-center gap-3 group hover"
+                  className="flex items-center gap-3 group hover mb-4"
                 >
                   <Avatar className="h-16 w-16 group-hover:ring-1 group-hover:ring-primary group-hover:ring-offset-2 transition-all">
                     <AvatarFallback>NU</AvatarFallback>
@@ -149,6 +177,26 @@ export default function ViewReportPage() {
                     </p>
                   </div>
                 </Link>
+                {report?.involved_caregivers &&
+                  report?.involved_caregivers.length > 0 && (
+                    <div>
+                      <h2 className="text-gray-500">Involved residents:</h2>
+                      <div>
+                        {report.involved_caregivers.map((ir, index) => (
+                          <span key={ir.id}>
+                            <Link
+                              href={`/dashboard/residents/${ir.id}`}
+                              className="border-b border-dotted border-muted-foreground"
+                            >
+                              {ir.name}
+                            </Link>
+                            {index < report.involved_caregivers!.length - 1 &&
+                              ", "}
+                          </span>
+                        ))}
+                      </div>
+                    </div>
+                  )}
               </div>
             </div>
 

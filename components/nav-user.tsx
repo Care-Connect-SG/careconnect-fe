065c0971ffffea402d98c114086d1d2679460253
ng-jayson <e1075537@u.nus.edu>
2025-03-21 03:53:29 +0800

    fix: build

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -55,7 +55,10 @@ export function NavUser() {
               ) : (
                 <>
                   <Avatar className="h-8 w-8 rounded-lg">
-                    <AvatarImage src={user?.profile_picture} alt={user?.name} />
+                    <AvatarImage
+                      src={user?.profile_picture!}
+                      alt={user?.name}
+                    />
                     <AvatarFallback className="rounded-lg">
                       {user?.email.charAt(0)}
                     </AvatarFallback>
@@ -78,7 +81,7 @@ export function NavUser() {
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user?.profile_picture} alt={user?.name} />
+                  <AvatarImage src={user?.profile_picture!} alt={user?.name} />
                   <AvatarFallback className="rounded-lg">
                     {user?.email.charAt(0)}
                   </AvatarFallback>
